7d06a205e62098be2b55b0e05fe92e25b45ec37b
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-23 14:02:45 -0600

    add luvi.lib and windows build

---
+++ Make.bat
@@ -0,0 +1,10 @@
+@ECHO off
+
+IF NOT "x%1" == "x" GOTO :%1
+
+GOTO :build
+
+:build
+cmake -H. -Bbuild -G"Visual Studio 12 Win64"
+cmake --build build --config Release -- /maxcpucount
+COPY build/Release/sigar.dll .
